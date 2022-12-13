daf28262bf8aa027a22f386a23a9642b97309df7
ssyy0122 <z01039118740@gmail.com>
2022-12-13 23:11:52 +0900

    :fire: 안쓰는 import 삭제
    엔티티를 만드는 중 생긴 안쓰는 import

--- src/main/kotlin/com/iwfsg/board/domain/post/entity/Post.kt
+++ src/main/kotlin/com/iwfsg/board/domain/post/entity/Post.kt
@@ -1,7 +1,6 @@
 package com.iwfsg.board.domain.post.entity
 
 import com.iwfsg.board.domain.user.entity.User
-import net.bytebuddy.dynamic.loading.InjectionClassLoader.Strategy
 import java.time.ZonedDateTime
 import javax.persistence.*
 
