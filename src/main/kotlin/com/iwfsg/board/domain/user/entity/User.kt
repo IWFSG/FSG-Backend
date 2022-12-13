44a59883af6b6ab196e0e27fb9c580e461dda7f0
ssyy0122 <z01039118740@gmail.com>
2022-12-14 00:05:54 +0900

    :sparkles: user entity 생성
    user의 엔티티를 생성하였다

--- src/main/kotlin/com/iwfsg/board/domain/user/entity/User.kt
+++ src/main/kotlin/com/iwfsg/board/domain/user/entity/User.kt
@@ -1,16 +1,19 @@
 package com.iwfsg.board.domain.user.entity
 
 import java.util.StringJoiner
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.Id
+import javax.persistence.*
 
 @Entity
 class User (
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val idx: Long,
+
+    @Column(nullable = false, length = 8)
     val id: String,
+
+    @Column(nullable = false, length = 8)
     val password: String,
-    @Column()
+
+    @Column(nullable = false, length = 1)
     val name: String
 )
