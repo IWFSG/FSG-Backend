72d81747f85546f225e456cf466c4098913b5a14
ssyy0122 <z01039118740@gmail.com>
2022-12-14 00:39:51 +0900

    :sparkles: Comment entity 생성
    헤응

--- src/main/kotlin/com/iwfsg/board/domain/comment/entity/Comment.kt
+++ src/main/kotlin/com/iwfsg/board/domain/comment/entity/Comment.kt
@@ -1,4 +1,24 @@
 package com.iwfsg.board.domain.comment.entity
 
-class Comment {
-}
+import com.iwfsg.board.domain.post.entity.Post
+import com.iwfsg.board.domain.user.entity.User
+import java.time.ZonedDateTime
+import javax.persistence.*
+
+@Entity
+class Comment (
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val idx: Long,
+
+    @Column(nullable = false)
+    val content: String,
+
+    val createAT: ZonedDateTime,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    val user: User,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    val post: Post
+
+)
