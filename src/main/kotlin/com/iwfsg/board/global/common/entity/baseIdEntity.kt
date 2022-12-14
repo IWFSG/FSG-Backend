1bce4443f6694cffb22040b81197b66d0e7715f4
ssyy0122 <z01039118740@gmail.com>
2022-12-14 16:21:49 +0900

    :zap: abstract add
    g

--- src/main/kotlin/com/iwfsg/board/global/common/entity/baseIdEntity.kt
+++ src/main/kotlin/com/iwfsg/board/global/common/entity/baseIdEntity.kt
@@ -6,7 +6,7 @@ import javax.persistence.Id
 import javax.persistence.MappedSuperclass
 
 @MappedSuperclass
-class baseIdEntity {
+abstract class baseIdEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0
