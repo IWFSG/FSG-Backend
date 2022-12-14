78fd3956d101e83e079541cc29d20c0c419bb858
ssyy0122 <z01039118740@gmail.com>
2022-12-14 16:21:09 +0900

    :sparkles: baseIdEntity create
    g

--- src/main/kotlin/com/iwfsg/board/global/common/entity/baseIdEntity.kt
+++ src/main/kotlin/com/iwfsg/board/global/common/entity/baseIdEntity.kt
@@ -1,5 +1,13 @@
 package com.iwfsg.board.global.common.entity
 
-class baseIdEntity {
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.MappedSuperclass
 
+@MappedSuperclass
+class baseIdEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0
 }
