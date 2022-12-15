eb02d018eb8e99ec7e4265f32bdae2f0301c7d46
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-12-15 15:30:03 +0900

    :recycle: db에 snake case 적용
    baseTimeEntity에 적용

--- src/main/kotlin/com/iwfsg/board/global/common/entity/BaseTimeEntity.kt
+++ src/main/kotlin/com/iwfsg/board/global/common/entity/BaseTimeEntity.kt
@@ -2,6 +2,7 @@ package com.iwfsg.board.global.common.entity
 
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.ZonedDateTime
+import javax.persistence.Column
 import javax.persistence.EntityListeners
 import javax.persistence.MappedSuperclass
 import javax.persistence.PrePersist
@@ -9,6 +10,7 @@ import javax.persistence.PrePersist
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener::class)
 abstract class BaseTimeEntity {
+    @Column(name = "create_at")
     var createdAt: ZonedDateTime? = null
 
     @PrePersist
