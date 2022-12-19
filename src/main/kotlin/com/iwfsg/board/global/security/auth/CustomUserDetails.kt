4993b8ea8132004d21a031ab80eba5d1b15fb095
ssyy0122 <z01039118740@gmail.com>
2022-12-19 23:31:55 +0900

    :sparkles: userdetails 생성
    add

--- src/main/kotlin/com/iwfsg/board/global/security/auth/CustomUserDetails.kt
+++ src/main/kotlin/com/iwfsg/board/global/security/auth/CustomUserDetails.kt
@@ -1,4 +1,25 @@
 package com.iwfsg.board.global.security.auth
 
-class CustomUserDetails:  {
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+
+
+class CustomUserDetails(private var user:User): UserDetails {
+
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority>? {
+        return null
+    }
+
+    override fun getPassword(): String = user.password
+
+    override fun getUsername(): String = user.username
+
+    override fun isAccountNonExpired(): Boolean = true
+
+    override fun isAccountNonLocked(): Boolean = true
+
+    override fun isCredentialsNonExpired(): Boolean = true
+
+    override fun isEnabled(): Boolean = true
 }
