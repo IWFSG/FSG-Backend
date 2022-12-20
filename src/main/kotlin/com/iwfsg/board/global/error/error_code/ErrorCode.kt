c28c7d44a60568d9252ef695e2fb2b7e2ebed959
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-12-20 23:08:45 +0900

    :recycle: 에러 메시지
    간격 수정

--- src/main/kotlin/com/iwfsg/board/global/error/error_code/ErrorCode.kt
+++ src/main/kotlin/com/iwfsg/board/global/error/error_code/ErrorCode.kt
@@ -1,5 +1,5 @@
 package com.iwfsg.board.global.error.error_code
 
 enum class ErrorCode(val message: String, val status: Int) {
-    INTERNET_SERVER_ERROR("InternetServerError",500)
+    INTERNET_SERVER_ERROR("Internet Server Error",500)
 }
