6b8502379b15952e5d0a410773ed8c377ab8ea7f
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-12-20 23:06:02 +0900

    :sparkles: errorHandler생성
    basicException을 handler할 코드

--- src/main/kotlin/com/iwfsg/board/global/error/handler/ExceptionHandler.kt
+++ src/main/kotlin/com/iwfsg/board/global/error/handler/ExceptionHandler.kt
@@ -1,4 +1,18 @@
 package com.iwfsg.board.global.error.handler
 
+import com.iwfsg.board.global.error.error_code.ErrorCode
+import com.iwfsg.board.global.error.exception.BasicException
+import com.iwfsg.board.global.error.response.ErrorResponse
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
 class ExceptionHandler {
+    @ExceptionHandler(BasicException::class)
+    fun basicExceptionHandler(error: BasicException): ResponseEntity<ErrorResponse> {
+        val errorCode: ErrorCode = error.errorCode
+        return ResponseEntity(ErrorResponse(errorCode.message,errorCode.status), HttpStatus.valueOf(errorCode.status))
+    }
 }
