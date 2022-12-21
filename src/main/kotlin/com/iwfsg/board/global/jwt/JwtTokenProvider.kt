cb855be0aaa949e0ba8e9fa1577bbad45df006ca
ssyy0122 <z01039118740@gmail.com>
2022-12-21 12:08:48 +0900

    :sparkles: toktn 생성 함수 생성
    ㅇ

--- src/main/kotlin/com/iwfsg/board/global/jwt/JwtTokenProvider.kt
+++ src/main/kotlin/com/iwfsg/board/global/jwt/JwtTokenProvider.kt
@@ -1,13 +1,22 @@
 package com.iwfsg.board.global.jwt
 
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwt
+import io.jsonwebtoken.Jwts
+import org.apache.catalina.authenticator.SpnegoAuthenticator.AuthenticateAction
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Component
+import java.util.Date
+import java.util.Locale
+import javax.persistence.Id
 
 @Component
 class JwtTokenProvider(
     private val jwtProperties: JwtProperties,
     private val userDetailsService: UserDetailsService,
     ) {
+
     companion object{
         const val ACCESS_TYPE = "access"
         const val REFRESH_TYPE = "refresh"
@@ -15,4 +24,15 @@ class JwtTokenProvider(
         const val REFRESH_EXP = 60L * 60 * 24 * 7 // 1 weeks
         const val TOKEN_PREFIX = "Bearer "
     }
+    fun createToken(userId: String,type: String, secret: String,exp: Long): String {
+        val claims: Claims = Jwts.claims().setSubject(userId)
+        claims["userId"] = userId
+        return Jwts.builder()
+            .setHeaderParam("typ", "JWT")
+            .setClaims(claims)
+            .setIssuedAt(Date())
+            .setExpiration(Date(System.currentTimeMillis() + exp * 1000))
+            .compact()
+    }
+
 }
