a89f44542eb960d6c1889f9b4c7432cc9cc20289
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-12-25 21:43:40 +0900

    :sparkles: entity 맵핑
    ㅇㅇ

--- src/main/kotlin/com/iwfsg/board/domain/post/entity/Category.kt
+++ src/main/kotlin/com/iwfsg/board/domain/post/entity/Category.kt
@@ -1,9 +1,16 @@
 package com.iwfsg.board.domain.post.entity
 
 import com.iwfsg.board.global.common.entity.BaseIdEntity
+import org.hibernate.annotations.OnDelete
+import org.hibernate.annotations.OnDeleteAction
 import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.ManyToOne
 
 @Entity
 class Category (
-    val title: String
+    val title: String,
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    val category: Category
 ): BaseIdEntity()
