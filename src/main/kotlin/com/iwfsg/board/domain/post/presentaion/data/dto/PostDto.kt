2d0124b3a262d564da584a1c192268cd1f48dc42
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-12-25 21:29:11 +0900

    :recycle: dto값 추가

--- src/main/kotlin/com/iwfsg/board/domain/post/presentaion/data/dto/PostDto.kt
+++ src/main/kotlin/com/iwfsg/board/domain/post/presentaion/data/dto/PostDto.kt
@@ -3,6 +3,7 @@ package com.iwfsg.board.domain.post.presentaion.data.dto
 data class PostDto(
     val idx: Long,
     val title: String,
+    val content: String,
     val thumbnail: String,
     val views: Long,
     val likeCount: String
