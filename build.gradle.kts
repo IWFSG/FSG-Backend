7ab3302890a4741fc732639a1647c71ca7b19a1c
ssyy0122 <z01039118740@gmail.com>
2023-01-01 22:35:19 +0900

    :sparkles: Config 설정
    create

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,9 +20,10 @@ dependencies {
 
     //jwt
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
-    implementation("org.projectlombok:lombok:1.18.22")
+    implementation("org.projectlombok:lombok:1.18.24")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
--- src/main/kotlin/com/iwfsg/board/global/security/config/SecurityConfig.kt
+++ src/main/kotlin/com/iwfsg/board/global/security/config/SecurityConfig.kt
@@ -1,4 +1,28 @@
 package com.iwfsg.board.global.security.config
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
 class SecurityConfig {
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain{
+        http
+            .csrf().disable()
+            .formLogin().disable()
+            .cors().disable()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authorizeHttpRequests()
+            .anyRequest().authenticated();
+        return http.build()
+    }
 }
