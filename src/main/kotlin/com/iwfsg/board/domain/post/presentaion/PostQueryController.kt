f556b56a1fa8a59b4663073e6b00b086930e650b
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-01-02 20:50:53 +0900

    :recycle: mapping 명시
    ㅇㅇ

--- src/main/kotlin/com/iwfsg/board/domain/post/presentaion/PostQueryController.kt
+++ src/main/kotlin/com/iwfsg/board/domain/post/presentaion/PostQueryController.kt
@@ -8,10 +8,12 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("api/v1/post/query")
 class PostQueryController(
     private val postQueryConverter: PostQueryConverter,
     private val postQueryService: PostQueryService
