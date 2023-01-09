4c9f4338f5d73ad7191eaaa37aff8bf843dfd411
ssyy0122 <z01039118740@gmail.com>
2023-01-09 14:55:53 +0900

    :sparkles: Controller생성

--- src/main/kotlin/com/iwfsg/board/domain/user/presentation/UserController.kt
+++ src/main/kotlin/com/iwfsg/board/domain/user/presentation/UserController.kt
@@ -1,17 +1,25 @@
 package com.iwfsg.board.domain.user.presentation
 
+import com.iwfsg.board.domain.user.presentation.data.request.RegisterRequest
 import com.iwfsg.board.domain.user.service.UserAccountService
+import com.iwfsg.board.domain.user.utils.UserConverter
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("api/v1/auth")
-class UserController (
+class UserController(
+    private val userAccountService: UserAccountService,
+    private val userConverter: UserConverter
 ) {
     @PostMapping("/signup")
-    fun signup(): ResponseEntity<Void>{
-        return ResponseEntity.ok().body(null)
-    }
+    fun signup(@RequestBody @Valid registerRequest: RegisterRequest):ResponseEntity<Void> =
+        userConverter.toDto(registerRequest)
+            .let { userAccountService.register(it) }
+            .let { ResponseEntity.status(HttpStatus.CREATED).build() }
 }
